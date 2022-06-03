9eab22af441917defc82818069943abed214e373
Anders Eknert <anders@eknert.com>
2022-06-03 15:01:02 +0200

    Fix validation script
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- validate.sh
+++ validate.sh
@@ -2,11 +2,22 @@
 
 exit_code=0
 
+if [ $(which kube-review) ]; then
+    kr=$(which kube-review)
+else
+    kr=bin/kube-review
+fi
+
 for dir in ./*/; do
-    result=$(kube-review create "$dir"input.yaml | opa eval -f raw -I -d "$dir"policy.rego 'data.policy.allow')
+    if [ "$dir" == "./bin/" ]; then
+        continue
+    fi
+
+    result=$($kr create "$dir"input.yaml | opa eval -f raw -I -d "$dir"policy.rego 'data.policy.allow')
 
     if [ "$result" != "true" ]; then
         echo "Policy validation failure in $dir"
+        echo "Output: $result"
         exit_code=1
     fi
 done
